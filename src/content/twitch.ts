ab30c411401e5d430976dda536f5eca03774742a
qkitzero <qkitzero@gmail.com>
2025-11-26 15:21:56 +0900

    remove: playback rate (#24)

--- src/content/twitch.ts
+++ src/content/twitch.ts
@@ -6,8 +6,6 @@ const checkForAds = (() => {
   ];
 
   const VIDEO_SELECTOR = 'video';
-  const MAX_PLAYBACK_RATE = 2.0;
-  const NORMAL_PLAYBACK_RATE = 1.0;
   const MUTE_MESSAGE_TYPE = 'MUTE_TAB';
   const UNMUTE_MESSAGE_TYPE = 'UNMUTE_TAB';
 
@@ -22,18 +20,10 @@ const checkForAds = (() => {
         chrome.runtime.sendMessage({ type: MUTE_MESSAGE_TYPE });
         isMutedByExtension = true;
       }
-
-      if (videoElement.playbackRate !== MAX_PLAYBACK_RATE) {
-        videoElement.playbackRate = MAX_PLAYBACK_RATE;
-      }
     } else {
       if (isMutedByExtension) {
         chrome.runtime.sendMessage({ type: UNMUTE_MESSAGE_TYPE });
         isMutedByExtension = false;
-      }
-
-      if (videoElement && videoElement.playbackRate !== NORMAL_PLAYBACK_RATE) {
-        videoElement.playbackRate = NORMAL_PLAYBACK_RATE;
       }
     }
   };
